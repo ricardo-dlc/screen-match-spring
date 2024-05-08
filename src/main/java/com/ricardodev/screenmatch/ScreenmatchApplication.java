32bc8328f1fba1b7372bb3387b645b1df1c14bcb
Ricardo de la Cruz <the_phantom_racer@hotmail.com>
2024-05-08 18:21:42 -0500

    Get episode information

--- src/main/java/com/ricardodev/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/ricardodev/screenmatch/ScreenmatchApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ricardodev.screenmatch.model.EpisodeData;
 import com.ricardodev.screenmatch.model.SeriesData;
 import com.ricardodev.screenmatch.service.ApiConsuming;
 import com.ricardodev.screenmatch.service.DataConverter;
@@ -24,11 +25,17 @@ public class ScreenmatchApplication implements CommandLineRunner {
         String API_KEY = env.get("API_KEY");
         String apiEndpoint = "http://www.omdbapi.com/?apikey=%s&t=futurama".formatted(API_KEY);
 
-        DataConverter dataCOnverter = new DataConverter();
         ApiConsuming api = new ApiConsuming();
-        String json = api.getData(apiEndpoint);
+        DataConverter dataCOnverter = new DataConverter();
+        String json;
+
+        json = api.getData(apiEndpoint);
         SeriesData data = dataCOnverter.getData(json, SeriesData.class);
         System.out.println(data);
+
+        json = api.getData(apiEndpoint + "&season=1&episode=1");
+        EpisodeData episodeData = dataCOnverter.getData(json, EpisodeData.class);
+        System.out.println(episodeData);
     }
 
 }
--- src/main/java/com/ricardodev/screenmatch/model/EpisodeData.java
+++ src/main/java/com/ricardodev/screenmatch/model/EpisodeData.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record SeriesData(
+public record EpisodeData(
         @JsonAlias("Title") String title,
-        int totalSeasons,
-        @JsonAlias("imdbRating") String rating) {
+        @JsonAlias("Episode") int episodeNumber,
+        @JsonAlias("imdbRating") String rating,
+        @JsonAlias("Released") String releaseDate) {
+
 }
